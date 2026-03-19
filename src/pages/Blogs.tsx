b91585507d02e899bb132cf0330eff233e3494c7
Fritz Dutz <frederick@nap.vc>
2026-03-19 08:38:39 +0000

    Changes
    Co-authored-by: fritz-dutz <188693842+fritz-dutz@users.noreply.github.com>

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const blogs = [
   {
-    title: "the super analyst",
+    title: "The super analyst",
     emoji: "🪄",
     internalUrl: "/blogs/super-analyst",
   },
