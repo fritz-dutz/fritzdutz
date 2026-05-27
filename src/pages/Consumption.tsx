db9a710800f820828b7c32e83332e3bc6fffdd58
Fritz Dutz <frederick@nap.vc>
2026-05-27 11:51:33 +0000

    Changes
    Co-authored-by: fritz-dutz <188693842+fritz-dutz@users.noreply.github.com>

--- src/pages/Consumption.tsx
+++ src/pages/Consumption.tsx
@@ -10,7 +10,7 @@ const peopleIAdmire = [
 const readingAndListening = [
   { name: "NAP Log", url: "https://naplog.substack.com/", description: "" },
   { name: "Jannik Schilling", url: "https://www.jannikschilling.com/writing/", description: "" },
-  { name: "Uncapped with Jack Altman", url: "https://uncappedpod.com", description: "" },
+  { name: "Uncapped", url: "https://uncappedpod.com", description: "" },
   { name: "Lenny's Podcast", url: "https://www.lennysnewsletter.com/podcast", description: "" },
   { name: "Dialectic", url: "https://www.dialectic.fm/", description: "" },
 ];
